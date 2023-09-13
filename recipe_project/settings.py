81a7237ff1745de4d7a4f6aaaf91298d9ad1aa05
Schlumen <lumen.haendler@web.de>
2023-09-13 16:07:39 +0200

    Update STATIC

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+   BASE_DIR / 'static'
+]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
